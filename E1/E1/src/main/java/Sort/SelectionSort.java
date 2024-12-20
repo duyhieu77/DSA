8397a05687fd5abc3f4982af7c5520986226f053
duy hieu <hieu.hd.2458@aptechlearning.edu.vn>
2024-12-20 15:08:40 +0700

    Add files via upload
    bài 1 2 và 3

---
+++ E1/E1/src/main/java/Sort/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sort;
+
+import Entity.Student;
+import java.util.List;
+
+public class SelectionSort {
+    public static void sort(List<Student> students) {
+        int n = students.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (students.get(j).getScore() < students.get(minIndex).getScore()) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            if (minIndex != i) {
+                Student temp = students.get(minIndex);
+                students.set(minIndex, students.get(i));
+                students.set(i, temp);
+            }
+        }
+    }
+}
